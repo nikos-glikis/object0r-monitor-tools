918d42e5e769c77cf0fcee0c6b760917d6a9e5f9
nikos <nikosglikis@gmail.com>
2020-10-31 11:49:26 +0200

    - Added deploy command.

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 mvn install -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
+#mvn deploy -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
