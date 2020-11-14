a2e55559f9a277eb639488e4386e6cfa660c2852
nikos <nikosglikis@gmail.com>
2020-11-14 18:57:54 +0200

    - Added BaseHardDiskSmartTest

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
 mvn install -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
 #mvn deploy -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
 #to install locally for tests, simple run mvn install
---
+++ src/main/java/com/object0r/monitor/tools/base/BaseHardDiskSmartTest.java
@@ -0,0 +1,52 @@
+package com.object0r.monitor.tools.base;
+
+import com.object0r.toortools.os.OsCommandOutput;
+import com.object0r.toortools.os.OsHelper;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+abstract public class BaseHardDiskSmartTest extends BaseTest {
+    public BaseHardDiskSmartTest(BaseReporter reporter) {
+        super(reporter);
+    }
+
+    protected void checkHardDiskFailures(String ip, String user) {
+        try {
+            OsCommandOutput osCommandOutput = OsHelper.runRemoteCommand(ip, "lsblk | awk '{print $1}'", user, "/", "id_rsa");
+            if (osCommandOutput.getExitCode() != 0) {
+                errors.add("Error while checking hard disk failure (" + ip + ")");
+            } else {
+                Vector<String> drives = getDrivesFromText(osCommandOutput.getStandardOutput());
+                for (String drive : drives) {
+                    osCommandOutput = OsHelper.runRemoteCommand(ip, "smartctl /dev/" + drive + " -a", "root", "/", "id_rsa");
+                    if (osCommandOutput.getExitCode() != 0) {
+                        errors.add("Error while checking hard disk failure - drive (" + drive + "): " + osCommandOutput.getErrorOutput());
+                    } else {
+                        String output = osCommandOutput.getErrorOutput();
+                        if (!output.contains("SMART overall-health self-assessment test result: PASSED") || output.contains("FAILED!")) {
+                            errors.add("Smart drive has failed: (" + ip + "-/dev/" + drive + ") " + osCommandOutput.getStandardOutput() + "\n" + osCommandOutput.getErrorOutput());
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            errors.add("Error while checking failed hard disks - Exception (" + ip + "): " + e.toString());
+        }
+    }
+
+    public Vector<String> getDrivesFromText(String text) {
+        Vector<String> drives = new Vector<>();
+        Scanner sc = new Scanner(text);
+        boolean firstLine = true;
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            if (!line.startsWith("├─") && !line.startsWith("│") && !line.startsWith("└") && !firstLine) {
+                drives.add(line.trim());
+            }
+            firstLine = false;
+        }
+        return drives;
+    }
+}
