31cb49567ffdaeddf25363fc8ce4ebb9a6ee151b
nikos <nikosglikis@gmail.com>
2021-04-02 16:33:11 +0300

    Maven stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
+#gpg --keyserver http://keyserver.ubuntu.com:11371 --send-keys key
 mvn install -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
 #mvn deploy -DperformRelease=true -DcreateChecksum=true -Dgpg.passphrase=password
 #to install locally for tests, simple run mvn install
