a6ff4685dc4d92600a4b337200b631d02ff3b770
nikos <nikosglikis@gmail.com>
2021-07-14 09:43:26 +0300

    - Add another case to hard disk monitoring.

--- src/main/java/com/object0r/monitor/tools/base/BaseHardDiskSmartTest.java
+++ src/main/java/com/object0r/monitor/tools/base/BaseHardDiskSmartTest.java
@@ -6,43 +6,60 @@ import com.object0r.toortools.os.OsHelper;
 import java.util.Scanner;
 import java.util.Vector;
 
-abstract public class BaseHardDiskSmartTest extends BaseTest {
-    public BaseHardDiskSmartTest(BaseReporter reporter) {
+abstract public class BaseHardDiskSmartTest extends BaseTest
+{
+    public BaseHardDiskSmartTest(BaseReporter reporter)
+    {
         super(reporter);
     }
 
-    protected void checkHardDiskFailures(String ip, String user) {
-        try {
+    protected void checkHardDiskFailures(String ip, String user)
+    {
+        try
+        {
             OsCommandOutput osCommandOutput = OsHelper.runRemoteCommand(ip, "lsblk | awk '{print $1}'", user, "/", "id_rsa");
-            if (osCommandOutput.getExitCode() != 0) {
+            if (osCommandOutput.getExitCode() != 0)
+            {
                 errors.add("Error while checking hard disk failure (" + ip + ")");
-            } else {
+            }
+            else
+            {
                 Vector<String> drives = getDrivesFromText(osCommandOutput.getStandardOutput());
-                for (String drive : drives) {
+                for (String drive : drives)
+                {
                     osCommandOutput = OsHelper.runRemoteCommand(ip, "smartctl /dev/" + drive + " -a", "root", "/", "id_rsa");
-                    if (osCommandOutput.getExitCode() != 0) {
+                    if (osCommandOutput.getExitCode() != 0)
+                    {
                         errors.add("Error while checking hard disk failure - drive (" + drive + "): " + osCommandOutput.getErrorOutput());
-                    } else {
+                    }
+                    else
+                    {
                         String output = osCommandOutput.getErrorOutput();
-                        if (!output.contains("SMART overall-health self-assessment test result: PASSED") || output.contains("FAILED!")) {
+                        if (!output.contains("SMART overall-health self-assessment test result: PASSED") || output.contains("FAILED!"))
+                        {
                             errors.add("Smart drive has failed: (" + ip + "-/dev/" + drive + ") " + osCommandOutput.getStandardOutput() + "\n" + osCommandOutput.getErrorOutput());
                         }
                     }
                 }
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             errors.add("Error while checking failed hard disks - Exception (" + ip + "): " + e.toString());
         }
     }
 
-    public Vector<String> getDrivesFromText(String text) {
+    public Vector<String> getDrivesFromText(String text)
+    {
         Vector<String> drives = new Vector<>();
         Scanner sc = new Scanner(text);
         boolean firstLine = true;
-        while (sc.hasNext()) {
+        while (sc.hasNext())
+        {
             String line = sc.nextLine();
-            if (!line.startsWith("├─") && !line.startsWith("│") && !line.startsWith("└") && !firstLine) {
+            if (!line.startsWith("├─") && !line.startsWith("│") && !line.startsWith("└") && !firstLine && !line.startsWith("loop"))
+            {
                 drives.add(line.trim());
             }
             firstLine = false;
@@ -50,21 +67,29 @@ abstract public class BaseHardDiskSmartTest extends BaseTest {
         return drives;
     }
 
-    protected void runRaidTests(String ip, String device, int deviceCount) {
-        try {
+    protected void runRaidTests(String ip, String device, int deviceCount)
+    {
+        try
+        {
             OsCommandOutput osCommandOutput = OsHelper.runRemoteCommand(ip, "mdadm -D /dev/md/" + device, "root", "/", "id_rsa");
-            if (osCommandOutput.getExitCode() != 0) {
+            if (osCommandOutput.getExitCode() != 0)
+            {
                 errors.add("Error while checking raid tests.");
-            } else {
+            }
+            else
+            {
                 if (
                         !osCommandOutput.getStandardOutput().contains("Active Devices : " + deviceCount) ||
                                 !osCommandOutput.getStandardOutput().contains("Failed Devices : 0") ||
                                 !osCommandOutput.getStandardOutput().contains("Working Devices : " + deviceCount)
-                ) {
+                )
+                {
                     errors.add("Error while checking raid tests. Output: \n" + osCommandOutput.getStandardOutput());
                 }
             }
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
             errors.add("Error while checking raid tests: " + e.toString());
         }
