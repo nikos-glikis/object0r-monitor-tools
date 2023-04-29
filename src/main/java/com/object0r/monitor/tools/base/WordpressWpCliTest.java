21e28ca788fbcf8241e58bdbdadeefa4ccef6e00
Nikos Glikis <nikosglikis@gmail.com>
2023-04-29 16:09:09 +0300

    - Mark tests started,completed,shouldrun

--- src/main/java/com/object0r/monitor/tools/base/WordpressWpCliTest.java
+++ src/main/java/com/object0r/monitor/tools/base/WordpressWpCliTest.java
@@ -20,40 +20,41 @@ abstract public class WordpressWpCliTest extends BaseTest
             OsCommandOutput osCommandOutput = OsHelper.runRemoteCommand(getHost(), getPort(), "su -  " + getUser() + " -c 'cd " + getPath() + " && wp core update'", getSshUser(), getPath(), getPrivateKeyPath());
             if (osCommandOutput.getExitCode() != 0)
             {
-                throw new Exception("Some error happened while runni WpCli tests (update core), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update core), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
             }
             if (!osCommandOutput.getStandardOutput().contains("Success: WordPress is up to date."))
             {
-                throw new Exception("Some error happened while runni WpCli tests (update core), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update core), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
             }
 
             osCommandOutput = OsHelper.runRemoteCommand(getHost(), getPort(), "su - " + getUser() + " -c ' cd " + getPath() + " &&  wp plugin update --all'", getSshUser(), getPath(), getPrivateKeyPath());
             if (osCommandOutput.getExitCode() != 0)
             {
-                throw new Exception("Some error happened while runni WpCli tests (update plugin), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update plugin), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
             }
             if (!osCommandOutput.getStandardOutput().contains("Success: Plugin already updated."))
             {
-                throw new Exception("Some error happened while runni WpCli tests (update plugin), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update plugin), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
             }
 
             osCommandOutput = OsHelper.runRemoteCommand(getHost(), getPort(), "su - " + getUser() + " -c ' cd " + getPath() + " && wp theme update --all'", getSshUser(), getPath(), getPrivateKeyPath());
             if (osCommandOutput.getExitCode() != 0)
             {
-                throw new Exception("Some error happened while runni WpCli tests (update theme), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update theme), error code is not zero, error output is:" + osCommandOutput.getErrorOutput());
             }
             if (!osCommandOutput.getStandardOutput().contains("Success: Theme already updated."))
             {
-                throw new Exception("Some error happened while runni WpCli tests (update themes), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
+                throw new Exception(getTestName() + " - Some error happened while runni WpCli tests (update themes), error code is not zero, standard output is:" + osCommandOutput.getStandardOutput());
             }
         }
         catch (Exception e)
         {
             e.printStackTrace();
-            errors.add(getTestName() + " - Some error happened while runni WpCli tests:" + e.getMessage());
+            errors.add(getTestName() + " - Some error happened while running WpCli tests:" + e.getMessage());
         }
         return errors;
     }
+
 
     public int getAcceptableUpdatePluginsCount()
     {
