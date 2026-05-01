a8e04e1e837cabce6b7b7eb032181b0c09a2a076
Nikos Glikis <nikosglikis@gmail.com>
2026-05-01 13:01:28 +0300

    Changes

--- src/main/java/com/object0r/monitor/tools/reporters/EmailReporter.java
+++ src/main/java/com/object0r/monitor/tools/reporters/EmailReporter.java
@@ -14,7 +14,7 @@ public class EmailReporter extends BaseReporter
         {
             if (OsHelper.isLinux())
             {
-                SendEmailHelper.sendEmail(recipient, formatSubject("osm - " + subject, 500), body);
+                SendEmailHelper.sendEmail(recipient, formatSubject("osm - " + body, 500), body);
             }
             else
             {
@@ -30,11 +30,12 @@ public class EmailReporter extends BaseReporter
         {
             return "";
         }
+        subject = subject.replace("\n", " ");
         if (subject.length() <= maxLength)
         {
             return subject;
         }
-        return subject.substring(0, maxLength - 3) + "...";
+        return subject.substring(0, maxLength);
     }
 
     protected boolean verifyRecipient(String recipient)
